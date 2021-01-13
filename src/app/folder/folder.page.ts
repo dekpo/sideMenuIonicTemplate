e86df9c23ddb8f0f6398a2af69c5f5ac2dd297b7
Dekpo Yologaza <dekpo@me.com>
2021-01-13 11:34:10 +0100

    Added all the stuff...

--- src/app/folder/folder.page.ts
+++ src/app/folder/folder.page.ts
@@ -7,12 +7,26 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./folder.page.scss'],
 })
 export class FolderPage implements OnInit {
-  public folder: string;
+
+  // public folder: string;
+  // on déclare nos propriétées
+  folder = "";
+  name = "";
 
   constructor(private activatedRoute: ActivatedRoute) { }
 
   ngOnInit() {
-    this.folder = this.activatedRoute.snapshot.paramMap.get('id');
+
+    // this.folder = this.activatedRoute.snapshot.paramMap.get('id');
+
+    this.activatedRoute.params.subscribe( (params) => {
+
+      console.log('params',params);
+      // on charge les paramètres dans les propriétées
+      this.folder = params.id;
+      this.name = params.name;
+    });
+
   }
 
 }
